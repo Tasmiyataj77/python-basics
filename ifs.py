acf6d64cb6d372ab63865704378b4ff6e11d5d0a
Tasmiya Taj <tasmiya.yaseen2000@gmail.com>
2025-05-26 21:54:50 +0530

    Create ifs.py

---
+++ ifs.py
@@ -0,0 +1,9 @@
+#if= do some code only if some condition is True else do something else
+
+
+age= int(input("enter your age: "))
+
+if age>=18:
+    print("you are now signed up!")
+else:
+    print("you must be 18+ to sign up")
