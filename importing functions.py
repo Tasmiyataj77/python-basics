2ed4337a9bc3dce4a4ef3110db275efbfb79d741
Tasmiya Taj <tasmiya.yaseen2000@gmail.com>
2025-05-26 21:55:39 +0530

    Create importing functions.py

---
+++ importing functions.py
@@ -0,0 +1,11 @@
+import math
+
+x=9
+print(math.pi)
+print(math.e)
+result=math.sqrt(x)
+y=4.1
+result=math.ceil(y) #rounding of to nearest  greater number
+z=6.8
+result=math.floor(z) #rounding of to integer part in the floating value
+print(result)
