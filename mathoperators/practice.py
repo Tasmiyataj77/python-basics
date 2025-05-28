6e7de2b74082b1a3cf319d1e3bdacbe19dfceddf
Tasmiya Taj <tasmiya.yaseen2000@gmail.com>
2025-05-28 22:21:59 +0530

    Create practice.py

---
+++ mathoperators/practice.py
@@ -0,0 +1,33 @@
+a=2 + 3 * 6
+print(a)
+
+
+b=(2+3)*6
+print(b)
+
+
+c=48565878 * 578453
+print(c)
+
+d= 2 ** 8
+print(d)
+
+
+e=23 / 7
+print(e)
+
+
+f=23 // 7
+print(f)
+
+
+g=23 % 7
+print(g)
+
+
+h=2+2
+print(h)
+
+
+i=(5 - 1) * ((7 + 1) / (3 - 1))
+print(i)
