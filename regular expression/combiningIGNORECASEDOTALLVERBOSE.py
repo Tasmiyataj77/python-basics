2b7c6c90c0fbbc7d5581ff7689c66e5eedbc3314
Tasmiya Taj <tasmiya.yaseen2000@gmail.com>
2025-07-01 23:09:16 +0530

    Add files via upload

---
+++ regular expression/combiningIGNORECASEDOTALLVERBOSE.py
@@ -0,0 +1,4 @@
+#Combining re.IGNORECASE, re.DOTALL, and re.VERBOSE
+import re
+someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL)
+someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL | re.VERBOSE)
