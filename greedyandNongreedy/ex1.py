932b889d87aee1252fb16ce00082aede81cf2f7b
Tasmiya Taj <tasmiya.yaseen2000@gmail.com>
2025-07-07 23:25:59 +0530

    Create ex1.py

---
+++ greedyandNongreedy/ex1.py
@@ -0,0 +1,11 @@
+import re
+#greedy method 
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+
+
+#non greedy method
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group())
