a236c05235aeab74585225e54facff8395b20dd7
Tasmiya Taj <tasmiya.yaseen2000@gmail.com>
2025-07-16 18:54:02 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+#exercise1 calculate the rectangle area 
+#a=wl or length*breadth
+length=int(input("enter the length: ")) 
+breadth=int(input("enter thr breadt: "))
+area= length*breadth
+print(f"the area is: {area}cm")
